fb8773e5e313dce35a99bca2b6f6eb800d204be0
Kei-TAKAHATA <184033287+Kei-TAKAHATA@users.noreply.github.com>
2025-04-26 15:53:56 +0900

    [feature]ToDoリストの作成

---
+++ frontend/src/components/TaskItem/styles.ts
@@ -0,0 +1,17 @@
+
+export const StyledTaskItem = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px',
+    borderBottom: '1px solid #ddd',
+  }
+
+  export const StyledButton = {
+    padding: '5px 10px',
+    backgroundColor: 'red',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  }
